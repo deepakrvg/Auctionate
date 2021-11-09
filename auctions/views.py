edbb889fa0eba0b0d40651c44655ba42c767737a
deepakrvg <deepakrvg@github.com>
2021-11-09 18:46:38 +0530

    intital commit to main branch

--- auctions/views.py
+++ auctions/views.py
@@ -85,6 +85,7 @@ def register(request):
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
+
     else:
         return render(request, "auctions/register.html")
 
